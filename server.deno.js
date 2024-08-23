3a58d796533c925bbddee267ad2c8fe44c21b65c
t-motegi <motegi.web@gmail.com>
2024-08-23 11:19:12 +0900

    ✨ ホーム画面の作成

--- server.deno.js
+++ server.deno.js
@@ -1,17 +1,17 @@
-import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
+// import { serveDir } from "https://deno.land/std@0.151.0/http/file_server.ts";
 
-Deno.serve(async (req) => {
-  const pathname = new URL(req.url).pathname;
-  console.log(pathname);
+// Deno.serve(async (req) => {
+//   const pathname = new URL(req.url).pathname;
+//   console.log(pathname);
 
-  if (req.method === "GET" && pathname === "/welcome-message") {
-    return new Response("jigインターンへようこそ！");
-  }
+//   if (req.method === "GET" && pathname === "/welcome-message") {
+//     return new Response("jigインターンへようこそ！");
+//   }
 
-  return serveDir(req, {
-    fsRoot: "public",
-    urlRoot: "",
-    showDirListing: true,
-    enableCors: true,
-  });
-});
+//   return serveDir(req, {
+//     fsRoot: "public",
+//     urlRoot: "",
+//     showDirListing: true,
+//     enableCors: true,
+//   });
+// });
