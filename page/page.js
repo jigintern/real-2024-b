d6ad9e8ee9289d5c537ce880548ff635f44e52fd
sp2lc-kobayashi <146934536+sp2lc-kobayashi@users.noreply.github.com>
2024-08-26 14:04:41 +0900

    QRの読み取り、作成機能完成
    public下にreadQR.html,makeQR.htmlを追加

--- page/page.js
+++ page/page.js
@@ -2,6 +2,6 @@
  * ロードが終わったら 「GET /home-page」でサーバーにアクセスする
  */
 window.onload = async () => {
-    const response = await fetch('/home-page');
-    document.querySelector('#home-page-title').innerText = await response.text();
-}
+  const response = await fetch("/home-page");
+  document.querySelector("#home-page-title").innerText = await response.text();
+};
