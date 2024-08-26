e5dcf564c4776699100f3496bf8d0520adf58859
KKNg <132037098+KKNg0-0@users.noreply.github.com>
2024-08-26 11:01:04 +0900

    Delete page directory

--- page/page.js
+++
@@ -1,7 +0,0 @@
-/**
- * ロードが終わったら 「GET /home-page」でサーバーにアクセスする
- */
-window.onload = async () => {
-    const response = await fetch('/home-page');
-    document.querySelector('#home-page-title').innerText = await response.text();
-}
