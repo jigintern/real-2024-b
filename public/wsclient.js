62f60533eb5e6a495744c8a252b60b67e6fee8c9
kanadani <kanadani@jig.jp>
2024-08-27 17:50:09 +0900

    👍 update optionalやめてみた

--- public/wsclient.js
+++ public/wsclient.js
@@ -4,7 +4,7 @@ const socket = new WebSocket(
   `ws://${uri.hostname}:8080/start_web_socket?username=${myUsername}`, // put username from url
 );
 
-socket?.onmessage = (m) => {
+socket.onmessage = (m) => {
   // 接続したときの処理
   const data = JSON.parse(m.data);
 
@@ -29,7 +29,7 @@ socket?.onmessage = (m) => {
 
 // sendPair(自分の名前、相手の名前)でサーバに送信する
 function sendPair(myName, pairName) {
-  socket?.send(
+  socket.send(
     JSON.stringify({
       event: "matching-request",
       myName: myName,
