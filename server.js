8e89e499ea8045c36fb6bb278f253f7884f580a0
kanadani <kanadani@jig.jp>
2024-08-27 18:12:08 +0900

    👍 update 接続時にメッセージを返すように

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { serve } from "http/server.ts";
 // https://deno.land/std@0.194.0/http/file_server.ts?s=serveDir
 import { serveDir } from "http/file_server.ts";
 
-const waitingList = new Map();  
+const waitingList = new Map();
 const clientsMap = new Map();   // all clients
 /**
  * APIリクエストを処理する
@@ -18,27 +18,28 @@ Deno.serve({
       socket.onopen = () => {
         // 接続したときの処理
         console.log("CONNECTED");
+        socket.send("connected");
       };
       socket.onmessage = (message) => {
         const data = JSON.parse(message.data);
         // 受信したときの処理
         switch (data.event) {
           // 送ってきた“もの”のイベント類
-          case "matching-request": 
+          case "matching-request":
             clientsMap.set(data.myName, socket);
             console.log(`matching-request received! user-data: ${data.myName},${data.pairName}`);
             const previousName = waitingList.get(data.pairName);  // get previous user's name
-            if((previousName != null) && (previousName === data.myName)){
+            if ((previousName != null) && (previousName === data.myName)) {
               // マッチングに成功した時の処理
-              const json = JSON.stringify({event: "matching-success"});
+              const json = JSON.stringify({ event: "matching-success" });
               const clientA = clientsMap.get(data.myName);
               clientA.send(json);
               const clientB = clientsMap.get(data.pairName);
               clientB.send(json);
-            }else{
+            } else {
               // マッチング待ちの時の処理
               waitingList.set(data.myName, data.pairName);
-              const json = JSON.stringify({event: "send-success"});
+              const json = JSON.stringify({ event: "send-success" });
               socket.send(json);
             }
             break;
@@ -48,12 +49,12 @@ Deno.serve({
         }
       };
       socket.onclose = () => console.log("DISCONNECTED"); // 接続が終わったときの処理
-      
+
       socket.onerror = (error) => console.error("ERROR:", error); // エラーが発生したときの処理
 
       return response;
-  
-    }else{
+
+    } else {
       const pathname = new URL(req.url).pathname;
       console.log(pathname);
 
